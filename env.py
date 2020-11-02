cfa3e0c82932f37bf3d74c4ef25dcfcb2f1328eb
Natte-Z <nathalie.zauels@gmail.com>
2020-11-02 10:27:11 +0000

    remove env.py

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRETKEY", "Gbi)AFpWXapaE$udcvqh4uh4~LVfD^" )
-os.environ.setdefault("MONGO_URI", "")
-os.environ.setdefault("MONGO_DBNAME", "p_earthling")
