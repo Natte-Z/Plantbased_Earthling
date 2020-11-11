0a35a7a1f913e0b819bab6397d07c431894c92ef
Natte-Z <nathalie.zauels@gmail.com>
2020-11-11 13:27:14 +0000

    Add recipe form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
     $('input#input_text, textarea#textarea2').characterCounter();
+    $('select').formSelect();
   });
 
 
