07c9ec58b31cc467fae0120295a807f76b479f3c
Natte-Z <nathalie.zauels@gmail.com>
2020-11-11 16:21:54 +0000

    Add categories dropdown

--- app.py
+++ app.py
@@ -99,7 +99,8 @@ def logout():
 
 @app.route("/add_recipe")
 def add_recipe():
-    return render_template("add_recipe.html")
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add_recipe.html", categories=categories)
 
 
 # always leave in the end 
